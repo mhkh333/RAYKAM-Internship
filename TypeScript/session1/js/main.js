800173cda637ecc187b60db31fd0a61f90c54759
Mohammad Hossein Khorrami <mohammadkhoram3@gmail.com>
2022-07-24 17:11:52 +0430

    typescript basics

---
+++ TypeScript/session1/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let data = (firstname, lastname, ...info) => {
+    console.log(info);
+};
+data("ss", 'khora', 'tehran', 'ray', "33", 555);
---
+++ TypeScript/session1/typeScript/main.ts
@@ -0,0 +1,5 @@
+let data=(firstname:string, lastname:string, ...info:(number|string)[]):void=>{
+    console.log(info);
+};
+
+data("ss",'khora', 'tehran', 'ray', "33", 555);
